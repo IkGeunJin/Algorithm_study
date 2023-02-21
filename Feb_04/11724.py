0c1975db701bd4838c867198eb9fddeb0a765918
IkGeunJin <ikthegreat@icloud.com>
2023-02-21 23:11:02 +0900

    11724 Update

--- Feb_04/11724.py
+++ Feb_04/11724.py
@@ -20,17 +20,21 @@ for s in range(1, V + 1):  # 1번 노드부터 마지막 노드까지 확인을 
     stack = []  # 빈 스택 생성
     if visited[s] == 0:  # 방문하지 않았을 경우에만 방문 표시 (조건문 달지 않으면 무조건 방문표시를 해버림)
         visited[s] = count  # 1 대신 카운트 변수를 통한 표시
-    while True:
-        for e in route[s]:  # s 에서 갈 수 있는 모든 e의 경우에서 (본인은 방문표시를 했으므로 무시)
-            if visited[e] == 0:  # 방문하지 않았다면
-                stack.append(s)  # 막힌 경우 돌아오기 위해 스택에 넣어두고
-                visited[e] = count  # count 변수를 통한 방문 표시 후
-                s = e  # 출발점 재설정
-        else:  # 모든 경우를 다 방문한 경우
-            if stack:  # 다른 경우가 있을 때 까지 스택을 pop 한다.
-                stack.pop()
-            else:  # 스택이 다 비워진 경우
-                count += 1  # count 변수에 1을 더한 후 (다른 연결 요소 구분을 위함)
-                break  # while 문 종료하고 2번 노드부터 끝까지 반복
+        while True:
+            for e in route[s]:  # s 에서 갈 수 있는 모든 e의 경우에서 (본인은 방문표시를 했으므로 무시)
+                if visited[e] == 0:  # 방문하지 않았다면
+                    stack.append(s)  # 막힌 경우 돌아오기 위해 스택에 넣어두고
+                    visited[e] = count  # count 변수를 통한 방문 표시 후
+                    s = e  # 출발점 재설정
+            else:  # 모든 경우를 다 방문한 경우
+                if stack:  # 다른 경우가 있을 때 까지 스택을 pop 한다.
+                    stack.pop()
+                else:  # 스택이 다 비워진 경우
+                    count += 1  # count 변수에 1을 더한 후 (다른 연결 요소 구분을 위함)
+                    break  # while 문 종료하고 2번 노드부터 끝까지 반복
+
+    else:  # 이미 방문한 노드인 경우 단계를 건너뛴다 (시간 단축)
+        continue
+
 
 print(len(set(visited))-1)  # set 으로 중복을 제거하여 연결 요소의 수량 확인 (0의 경우 -1을 채워뒀기 때문에 -1 해준다)
